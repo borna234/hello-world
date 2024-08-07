086132933195948d46c5a9f44eaff825d6ea1aaf
borna234 <borna.svjetlicic3@gmail.com>
2024-08-07 08:37:52 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+app.listen(3000, ()=>{
+    console.log("Server running on port 3000...");
+});
+
+app.get("/", (req, res) => {
+    res.send("go to link /data.txt");
+});
+
+app.get("/data", (req, res) =>{
+    res.download("proba.txt");
+});
