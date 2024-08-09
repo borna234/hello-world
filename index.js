3ea513bbc6d9822c951aa97fb8df6baefbe52847
borna <borna.svjetlicic@gmail.com>
2024-08-09 14:17:06 +0200

    commited index.js

--- index.js
+++ index.js
@@ -1,18 +1,6 @@
 const express = require("express");
+const os = require("os");
+
 const app = express();
-app.listen(3000, ()=>{
-    console.log("Server running on port 3000...");
-});
+console.log("h");
 
-app.get("/", (req, res) => {
-    res.send("go to link /data.txt");
-});
-
-app.get("/data", (req, res) =>{
-    res.download("proba.txt");
-});
-
-app.get("/data2", (req, res) =>{
-    res.send("otvorili ste stranicu");
-});
-//vfnvjfvfjvf
